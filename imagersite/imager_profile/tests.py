037c0eb517f76ed89cbd00805d9eaca3c9ef60e6
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-19 16:15:43 -0700

    added first test.

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,39 @@
+"""Tests for imager profile app."""
 from django.test import TestCase
+from django.contrib.auth.models import User
+from imager_profile.models import ImagerProfile
 
-# Create your tests here.
+import factory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """Make fake users."""
+
+    class Meta:
+        """Set fake users name and email."""
+
+        model = User
+    username = factory.Sequence(lambda n: 'user{}'.format(n))
+    email = factory.Sequence(
+        lambda n: 'user{}@cookies.com'.format(n)
+    )
+
+
+class ProfileTestCase(TestCase):
+    """Testing starts here... for the model at least."""
+
+    def setUp(self):
+        """Create the fake users."""
+        users = [UserFactory.create() for i in range(25)]
+
+        for user in users:
+            user.set_password('cake')
+            user.save()
+
+        self.users = users
+
+    def test_users_have_profiles(self):
+        """The function says it all."""
+        with self.assertRaises(Exception):
+            profile = ImagerProfile()
+            profile.save()
