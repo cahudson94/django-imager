d4e373c87591e6cb7eba7f5e0b947d61eaee4e9c
Chris Hudson <c.ahudson84@yahoo.com>
2017-07-13 15:05:09 -0700

    hopefully not merge conflicts, working on finishing step 2 stuff.

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -2,8 +2,7 @@
 from django.shortcuts import render
 from imager_profile.models import ImagerProfile
 from imager_images.models import ImagerPhoto, ImagerAlbum
-# from django.http import HttpResponse
-# from django.template import loader
+from django.contrib.auth.models import User
 
 
 def home_view(request):
@@ -21,6 +20,18 @@ def profile_view(request):
     """The view for our profile page."""
     current_user = request.user
     user = ImagerProfile.objects.filter(user=current_user).first()
+    pub_pics = (ImagerPhoto.objects
+                .filter(user=current_user)
+                .filter(published='PB')).count()
+    pub_albums = (ImagerAlbum.objects
+                  .filter(user=current_user)
+                  .filter(published='PB')).count()
+    prv_pics = (ImagerPhoto.objects
+                .filter(user=current_user)
+                .filter(published='PV')).count()
+    prv_albums = (ImagerAlbum.objects
+                  .filter(user=current_user)
+                  .filter(published='PV')).count()
     userdata = {'username': user.user,
                 'location': (user.city + ', ' + user.state),
                 'pic': user.pic,
@@ -28,12 +39,41 @@ def profile_view(request):
                 'camera': user.camera_type,
                 'photostyle': user.photography_style,
                 'website': user.website,
-                'pub_pics': user.pub_pics,
-                'pub_albums': user.pub_albums,
-                'prv_pics': user.prv_pics,
-                'prv_albums': user.prv_albums,
+                'pub_pics': pub_pics,
+                'pub_albums': pub_albums,
+                'prv_pics': prv_pics,
+                'prv_albums': prv_albums,
                 }
     return render(request, 'imagersite/profile.html', context=userdata)
+
+
+def user_profile_view(request, request_username):
+    """The view for another users profile."""
+    user = request_username.lower()
+    request_user = User.objects.filter(username=user)
+    profile = ImagerProfile.objects.filter(user=request_user)
+    pub_pics = (ImagerPhoto.objects
+                .filter(user=request_user)
+                .filter(published='PB'))
+    pub_albums = (ImagerAlbum.objects
+                  .filter(user=request_user)
+                  .filter(published='PB'))
+    return render(
+        request,
+        'imagersite/other_profile.html',
+        context={
+            'user': user,
+            'username': profile.user,
+            'location': (profile.city + ', ' + profile.state),
+            'pic': profile.pic,
+            'job': profile.job,
+            'camera': profile.camera_type,
+            'photostyle': profile.photography_style,
+            'website': profile.website,
+            'pub_pics': pub_pics,
+            'pub_albums': pub_albums,
+        }
+    )
 
 
 def library_view(request):
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from imager_profile.views import (
     home_view,
     profile_view,
+    user_profile_view,
     library_view,
     photo_view,
     album_view)
@@ -33,6 +34,8 @@ urlpatterns = [
     url(r'^login/', auth_views.login, name='login'),
     url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^profile/', profile_view, name='profile'),
+    url(r'^profile/(?P<request_username>\w+\d+)/$',
+        user_profile_view, name='public_profile'),
     url(r'^images/library/', library_view, name='library'),
     url(r'^images/photos/(?P<photo_id>\d+)/$', photo_view, name='photo'),
     url(r'^images/albums/(?P<album_id>\d+)/$', album_view, name='album')
