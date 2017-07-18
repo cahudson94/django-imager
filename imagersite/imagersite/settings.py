dc85257a93f5fc9858cf7c73de89b42f4e22772e
chud <c.ahudson84@yahoo.com>
2017-07-18 16:32:52 -0700

    missed the location field.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'location_field.apps.DefaultConfig',
     'imager_profile',
     'imagersite',
     'imager_images',
