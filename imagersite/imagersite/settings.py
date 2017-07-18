b0f5e76efac12dee2fb769c2f974006174cf63d5
chud <c.ahudson84@yahoo.com>
2017-07-18 16:38:46 -0700

    testbd issues.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'HOST': os.environ.get('DATABASE_HOST', ''),
         'PORT': '5432',
         'TEST': {
-            'NAME': 'testdb'
+            'NAME': 'test_db'
         },
     }
 }
